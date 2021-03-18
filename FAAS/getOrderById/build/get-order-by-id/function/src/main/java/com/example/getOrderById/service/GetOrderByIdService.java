0e301383654ded1713b591d3562c1fab231a0475
Kanakami <979466793@qq.com>
2021-03-18 13:38:45 +0800

    update mongodb for get-order-by-id

--- FAAS/getOrderById/build/get-order-by-id/function/src/main/java/com/example/getOrderById/service/GetOrderByIdService.java
+++
@@ -1,8 +0,0 @@
-package com.example.getOrderById.service;
-
-import edu.fudan.common.util.Response;
-import org.springframework.http.HttpHeaders;
-
-public interface GetOrderByIdService {
-    Response getOrderById(String orderId, HttpHeaders headers);
-}
